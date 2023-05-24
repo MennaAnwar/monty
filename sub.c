fdc215b2b06d74b7e8a6c0c4dbf04499922fb0d7
MennaAnwar <menna_m20@yahoo.com>
2023-05-24 18:21:41 +0300

    subtract

---
+++ sub.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+  *sub- sustration
+  *@head: stack head
+  *@line_number line_number
+  *Return: no return
+ */
+void sub(stack_t **head, unsigned int line_number)
+{
+	stack_t *current;
+	int result, len = 0;
+
+	current = *head;
+	len = stack_size(*head);
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		terminate(*head);
+	}
+
+	result = current->next->n - current->n;
+	current->next->n = result;
+	*head = current->next;
+	free(current);
+}
